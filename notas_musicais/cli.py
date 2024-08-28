49ba11d783db3402d04eb0a79df98db30ad319b8
Leonardo Jacomussi <leonardojacomussi@gmail.com>
2024-08-28 10:05:20 -0300

    feat: add CLI command for chords

--- notas_musicais/cli.py
+++ notas_musicais/cli.py
@@ -2,6 +2,7 @@ from rich.console import Console
 from rich.table import Table
 from typer import Argument, Typer
 
+from notas_musicais.acordes import acorde as _acorde
 from notas_musicais.escalas import escala as _escala
 
 console = Console()
@@ -24,4 +25,20 @@ def escala(
         table.add_column(grau)
 
     table.add_row(*notas)
+    console.print(table)
+
+
+@app.command()
+def acorde(
+    cifra: str = Argument('C', help='Cifra de um acorde'),
+):
+    table = Table(title='Acorde de {}'.format(cifra.upper()))
+
+    notas, graus = _acorde(cifra).values()
+
+    for grau in graus:
+        table.add_column(grau)
+
+    table.add_row(*notas)
+
     console.print(table)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,23 +7,47 @@ runner = CliRunner()
 
 
 def test_escala_cli_should_return_0_from_stdout():
-    result = runner.invoke(app)
+    result = runner.invoke(app, ['escala'])
     assert result.exit_code == 0
 
 
 @mark.parametrize('nota', ['C', 'D', 'E', 'F', 'G', 'A', 'B'])
 def test_escala_cli_should_contain_notes_in_response(nota):
-    result = runner.invoke(app)
+    result = runner.invoke(app, ['escala'])
     assert nota in result.stdout
 
 
 @mark.parametrize('nota', ['F', 'G', 'A', 'A#', 'C', 'D', 'E'])
 def test_escala_cli_should_contain_notes_in_response_of_fa(nota):
-    result = runner.invoke(app, ['F'])
+    result = runner.invoke(app, ['escala', 'F'])
     assert nota in result.stdout
 
 
 @mark.parametrize('grau', ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'])
 def test_escala_cli_should_contain_all_degrees(grau):
-    result = runner.invoke(app, ['F'])
+    result = runner.invoke(app, ['escala', 'F'])
     assert grau in result.stdout
+
+
+@mark.parametrize('nota', ['C', 'E', 'G'])
+def test_acorde_cli_should_contain_notes_in_response(nota):
+    result = runner.invoke(app, ['acorde'])
+    assert nota in result.stdout
+
+
+@mark.parametrize('grau', ['I', 'III', 'V'])
+def test_acorde_cli_should_contain_all_degrees(grau):
+    result = runner.invoke(app, ['acorde', 'F'])
+    assert grau in result.stdout
+
+
+# @mark.parametrize('grau', ['I', 'ii', 'iii', 'IV', 'V', 'vi', 'vii°'])
+# def test_campo_harmonico_cli_should_contain_all_degrees(grau):
+#     result = runner.invoke(app, ['campo-harmonico', 'C'])
+#     assert grau in result.stdout
+
+
+# @mark.parametrize('cifra', ['C', 'Dm', 'Em', 'F', 'G', 'Am', 'B°'])
+# def test_campo_harmonico_cli_should_contain_all_chords(cifra):
+#     result = runner.invoke(app, ['campo-harmonico', 'C'])
+#     assert cifra in result.stdout
