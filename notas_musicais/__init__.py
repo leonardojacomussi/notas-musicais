f3616fad8a34b139780222608c6f2bba937f5f22
Leonardo Jacomussi <leonardojacomussi@gmail.com>
2024-08-28 22:52:22 -0300

    feat: update package version

--- notas_musicais/__init__.py
+++ notas_musicais/__init__.py
@@ -3,7 +3,7 @@ from .escalas import ESCALAS, NOTAS, escala
 from .acordes import acorde, semitom, triade
 from .campo_harmonico import campo_harmonico
 
-__version__ = '0.0.1'
+__version__ = '0.1.1'
 
 __all__ = [
     'acorde',
